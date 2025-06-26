4fd60826fea24d1af99925b628327588aae3b2c3
khil94 <zidru94@gmail.com>
2025-06-26 16:32:47 +0900

    style : main section 스타일 수정
    - image fill을 위해 container에 relative 삽입

--- src/sections/main/mainSection.tsx
+++ src/sections/main/mainSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function MainSection() {
   return (
-    <div className="w-full h-svh">
+    <div className="w-full h-screen relative">
       <h1 className="z-50 text-4xl text-white absolute top-1/2 left-[10%] flex flex-col space-y-4">
         <strong>신뢰를 넘어, 미래로</strong>
         <strong>KD SOLUTION</strong>
