bff026b022e2414045595cf2c4e2858307fe3ab7
khil94 <zidru94@gmail.com>
2025-06-26 17:22:01 +0900

    style : mainSection 문구 z-index 수정
    - navbar를 뚫지 않도록 수정

--- src/sections/main/mainSection.tsx
+++ src/sections/main/mainSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MainSection() {
   return (
     <div className="w-full h-screen relative">
-      <h1 className="z-50 text-4xl text-white absolute top-1/2 left-[10%] flex flex-col space-y-4">
+      <h1 className="z-10 text-4xl text-white absolute top-1/2 left-[10%] flex flex-col space-y-4">
         <strong>신뢰를 넘어, 미래로</strong>
         <strong>KD SOLUTION</strong>
       </h1>
