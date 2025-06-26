a6578ba31818d75390b463a2b51647ad2ea20d85
khil94 <zidru94@gmail.com>
2025-06-26 20:03:13 +0900

    feat : main page 지도 섹션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import LinkSection from "@/sections/main/linkSection";
 import MainSection from "@/sections/main/mainSection";
+import MapSection from "@/sections/main/mapSection";
 
 export default function Home() {
   return (
     <div>
       <MainSection />
       <LinkSection />
+      <MapSection />
     </div>
   );
 }
