6e2fcb944ad29f2f899f6fda8cdbb3a0977b119d
khil94 <zidru94@gmail.com>
2025-06-30 16:58:29 +0900

    feat : redirect 설정
    - 각 메뉴 클릭시 메뉴의 subitem의 첫번째 링크로 리다이렉팅

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/about/introduction",
+        permanent: true,
+      },
+      {
+        source: "/products",
+        destination: "/products/main",
+        permanent: true,
+      },
+      {
+        source: "/facilities",
+        destination: "/facilities/machinery",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/contact/location",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
