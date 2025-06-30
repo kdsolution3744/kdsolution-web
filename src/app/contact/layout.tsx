b7382d3ca8135f0e13f64ef46f4e59fab0a97822
khil94 <zidru94@gmail.com>
2025-06-30 19:46:02 +0900

    feat : 문의 layout

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,12 @@
+import SidebarLayout from "@/components/sidebarLayout";
+import { MENU_ITEM } from "@/constants/menu";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <SidebarLayout imgSrc="/about.png" targetItem={MENU_ITEM[3].subItems}>
+      {children}
+    </SidebarLayout>
+  );
+}
