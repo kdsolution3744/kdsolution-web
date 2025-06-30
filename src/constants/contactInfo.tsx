2a009244ddd3c6bcf194c96f884c3406722d8ced
khil94 <zidru94@gmail.com>
2025-06-30 22:49:01 +0900

    feat : contact 목업값

---
+++ src/constants/contactInfo.tsx
@@ -0,0 +1,36 @@
+import { House, Mail, Phone, Printer } from "lucide-react";
+import { ReactNode } from "react";
+
+export interface IContactInfo {
+  title: string;
+  content: string;
+  desc: string;
+  icon: ReactNode;
+}
+
+export const CONTACT_INFO: IContactInfo[] = [
+  {
+    title: "TEL",
+    content: "031-493-3744",
+    desc: "평일 09:00 - 18:00",
+    icon: <Phone />,
+  },
+  {
+    title: "FAX",
+    content: "031-493-3745",
+    desc: "평일 09:00 - 18:00",
+    icon: <Printer />,
+  },
+  {
+    title: "이메일",
+    content: "kds3744@naver.com",
+    desc: "24시간 접수 가능",
+    icon: <Mail />,
+  },
+  {
+    title: "주소",
+    content: "경기도 안산시 단원구 성곡로 176",
+    desc: "621호",
+    icon: <House />,
+  },
+];
