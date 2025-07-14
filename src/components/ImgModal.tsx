eb754f8c4d1b9ec0a01e07a4d99300e6917e3094
khil94 <zidru94@gmail.com>
2025-07-15 00:42:21 +0900

    style : imgModal styling
    - 유리효과
    - 상단 타이틀 삭제

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -49,20 +49,15 @@ export default function ImgModal({
         className="relative bg-gray-200/20 bg-clip-padding backdrop-filter 
                   backdrop-blur-sm border border-gray-100/30 
                   rounded-2xl shadow-2xl w-full max-w-2xl 
-                  min-w-[320px] min-h-[420px] max-h-[90vh] 
+                  min-w-[320px] min-h-[420px] max-h-[90vh] h-full 
                   flex flex-col overflow-hidden"
       >
-        {/* Title */}
-        <div className="w-full px-8 pt-6 pb-2 text-xl font-bold text-center border-b border-gray-200 bg-white z-10">
-          {imgSource.title}
-        </div>
-        {/* Close Button */}
         <button
           onClick={handleClose}
           className="absolute top-4 right-4 z-20 w-10 h-10 bg-opacity-90 hover:bg-opacity-100 rounded-full flex items-center justify-center transition-all duration-200 "
         >
           <svg
-            className="w-6 h-6 text-gray-600"
+            className="w-6 h-6 text-white hover:text-gray-400"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -75,9 +70,8 @@ export default function ImgModal({
             />
           </svg>
         </button>
-
         {/* Image Section */}
-        <div className="flex flex-col items-center justify-center p-8 space-y-4 ">
+        <div className="flex flex-col h-full items-center justify-center p-8 space-y-4 ">
           <div className="relative flex-1 flex w-full min-w-[300px] min-h-[400px] ">
             <Image
               src={imgSource.imageList[idx] || "/placeholder.svg"}
