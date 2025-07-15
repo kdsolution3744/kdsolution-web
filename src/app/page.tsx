5bdfad5b2fad35c49a70772e4b7b2acda077a938
khil94 <zidru94@gmail.com>
2025-07-15 21:30:21 +0900

    chore : 통계 섹션 제외

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import ContactSection from "@/sections/main/contactSection";
 import LinkSection from "@/sections/main/linkSection";
 import MainSection from "@/sections/main/mainSection";
-import StatisticsSection from "@/sections/main/statisticsSection";
 
 export default function Home() {
   return (
     <div>
       <MainSection />
-      <StatisticsSection />
+      {/* <StatisticsSection /> */}
       <LinkSection />
       {/* <MapSection /> */}
       <ContactSection />
