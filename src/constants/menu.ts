c33a572245b59f2a9e0e068c7e97aa7c65b220eb
khil94 <zidru94@gmail.com>
2025-07-30 16:07:52 +0900

    test : AB테스트용

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -23,6 +23,7 @@ export const MENU_ITEM: IMenuList[] = [
     subItems: [
       { title: "소재", href: "/products/material" },
       { title: "가공품", href: "/products/products" },
+      { title: "가공품B", href: "/products/productB" },
     ],
   },
   {
