0163277806b9ef9b7cd2218eaf39296f54e80031
khil94 <zidru94@gmail.com>
2025-08-04 18:32:51 +0900

    chore : 회사소개, 소재 헤더 이미지 수정

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,11 +1,11 @@
-import aboutHead from "@/assets/헤더/회사소개3.webp";
+import aboutHead from "@/assets/헤더/회사소개.webp";
 import SidebarLayout from "@/components/sidebarLayout";
 import { MENU_ITEM } from "@/constants/menu";
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <SidebarLayout imgSrc={aboutHead} targetItem={MENU_ITEM[0].subItems}>
+    <SidebarLayout cover imgSrc={aboutHead} targetItem={MENU_ITEM[0].subItems}>
       {children}
     </SidebarLayout>
   );
